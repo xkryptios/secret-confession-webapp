0e9a422a32900e7c7a7e0725d200a981b6a2fe1c
XIANG <linruixiang123@gmail.com>
2020-03-31 13:31:08 +0800

    changed port setting for app listener

--- app.js
+++ app.js
@@ -220,6 +220,6 @@ app.get("/secrets", function(req, res) {
 	});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("server began at port 3000");
 });
